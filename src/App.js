8d6d2d27dff4b85ba5f34d5e991b69ee00459898
AkhilPolisetty <akhilpolisetty456@gmail.com>
2024-03-04 19:59:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from "react-bootstrap";
+import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <main className="py-3">
+      <Container>
+      <Outlet/>
+      </Container>
+    </main>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
