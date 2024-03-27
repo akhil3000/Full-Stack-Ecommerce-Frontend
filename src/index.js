dff88de60c0d1ac512051c431a602df64393a540
AkhilPolisetty <akhilpolisetty456@gmail.com>
2024-03-27 16:24:21 +0530

    March 27th change

--- src/index.js
+++ src/index.js
@@ -11,8 +11,12 @@ import{
   Route,
   RouterProvider
 } from 'react-router-dom'
+import {Provider} from 'react-redux';
+import store from './store';
 //import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
+
+
 const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
@@ -25,7 +29,9 @@ const router=createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>
 );
 
